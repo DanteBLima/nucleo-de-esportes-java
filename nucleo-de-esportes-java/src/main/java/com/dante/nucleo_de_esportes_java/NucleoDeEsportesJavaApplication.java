c1e6db7e5ceac9d8622d11ea4464ee3eae5644f2
Dante BLima <dante.borges@sempreceub.com>
2025-07-15 20:10:24 -0300

    first commit

---
+++ nucleo-de-esportes-java/src/main/java/com/dante/nucleo_de_esportes_java/NucleoDeEsportesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dante.nucleo_de_esportes_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleoDeEsportesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NucleoDeEsportesJavaApplication.class, args);
+	}
+
+}
---
+++ nucleo-de-esportes-java/src/test/java/com/dante/nucleo_de_esportes_java/NucleoDeEsportesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dante.nucleo_de_esportes_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleoDeEsportesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
