9914496d6cedee514df4bdd4ee875f1ba144d501
Dante BLima <dante.borges@sempreceub.com>
2025-09-23 22:40:04 -0300

    feat: getAllTurmas e delete turmaById

--- nucleo-de-esportes-java/src/main/java/com/dante/nucleo_de_esportes_java/controller/TurmaController.java
+++ nucleo-de-esportes-java/src/main/java/com/dante/nucleo_de_esportes_java/controller/TurmaController.java
@@ -6,16 +6,17 @@ import com.dante.nucleo_de_esportes_java.dto.TurmaResponseDto;
 import com.dante.nucleo_de_esportes_java.model.Turma;
 import com.dante.nucleo_de_esportes_java.repository.LocalRepository;
 import com.dante.nucleo_de_esportes_java.repository.ModalidadeRepository;
+import com.dante.nucleo_de_esportes_java.repository.TurmaRepository;
 import com.dante.nucleo_de_esportes_java.services.TurmaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("turma")
@@ -24,11 +25,32 @@ public class TurmaController {
     @Autowired
     TurmaService turmaService;
 
+    @Autowired
+    TurmaRepository turmaRepository;
+
 
     @PostMapping
     public ResponseEntity<TurmaResponseDto> createTurma (@RequestBody @Valid TurmaRequestDto data){
        TurmaResponseDto newTurma =  turmaService.registerTurma(data);
        return ResponseEntity.status(201).body(newTurma);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<TurmaResponseDto>> getAllTurmas(){
+        List<TurmaResponseDto> turmas = turmaService.getTurmas();
+        if (turmas.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(200).body(turmas);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTurmaById(@PathVariable (value = "id") Long id ){
+        Optional<Turma> turma = turmaRepository.findById(id);
+        if (turma.isEmpty()){
+            return ResponseEntity.status(404).body("Turma não encontrada");
+        }
+        turmaRepository.deleteById(id);
+        return ResponseEntity.status(200).body("Turma deletada com sucesso");
     }
 }
--- nucleo-de-esportes-java/src/main/java/com/dante/nucleo_de_esportes_java/services/TurmaService.java
+++ nucleo-de-esportes-java/src/main/java/com/dante/nucleo_de_esportes_java/services/TurmaService.java
@@ -12,6 +12,9 @@ import com.dante.nucleo_de_esportes_java.repository.TurmaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class TurmaService {
 
@@ -32,7 +35,6 @@ public class TurmaService {
 
         Modalidade modalidade = modalidadeRepository.findById(data.modalidade_id()).orElseThrow(() -> new RuntimeException("Modalidade nao encontrada"));
         Local local = localRepository.findById(data.local_id()).orElseThrow(() -> new RuntimeException("Local nao encontrado"));
-
 
 
         Turma newTurma = new Turma();
@@ -56,4 +58,24 @@ public class TurmaService {
                 createdTurma.getLocal().getName(),
                 createdTurma.getLimite_inscritos());
     }
+
+    public List<TurmaResponseDto> getTurmas() {
+        List<Turma> turmas = turmaRepository.findAll();
+
+        return turmas.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
+    private TurmaResponseDto convertToDto(Turma turma) {
+        return new TurmaResponseDto(
+                turma.getId(),
+                turma.getHorario_inicio(),
+                turma.getHorario_fim(),
+                turma.getDia_semana(),
+                turma.getModalidade().getName(),
+                turma.getLocal().getName(),
+                turma.getLimite_inscritos()
+        );
+    }
+
 }
